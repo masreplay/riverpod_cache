bfd4df8426e7fdbf1e17b32210811d9adb237ee9
Mas <masreplay@gmail.com>
2024-07-14 13:53:44 +0300

    chore: Refactor code structure and update package imports in main.dart and future_provider.dart

--- lib/src/future_provider.dart
+++ lib/src/future_provider.dart
@@ -37,7 +37,7 @@ extension FutureProviderOfflinePersistence on Ref {
     required String key,
     required Future<T> Function() future,
     required SharedPreferences sharedPreferences,
-    required T Function(Encodable json) fromJson,
+    required T Function(Encodable map) fromJson,
     required Encodable Function(T object) toJson,
   }) async* {
     T? getCachedResult() {
