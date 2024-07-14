7576f763e525d3b7758a7fd6a8dff5db04592658
Mas <masreplay@gmail.com>
2024-07-14 13:54:42 +0300

    chore: Update package imports and refactor code structure in riverpod_cache.dart

--- lib/riverpod_cache.dart
+++ lib/riverpod_cache.dart
@@ -1,5 +1,5 @@
 library riverpod_cache;
 
 export 'src/future_provider.dart';
-export 'src/preference_notifier_mixin.dart';
+export 'src/notifier_offline_persistence_mixin.dart';
 export 'src/stream_preference_notifier_mixin.dart';
